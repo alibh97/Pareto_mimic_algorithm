70a5aa4945bc9021f64a4eb1b895a3d571be15dd
alibh97 <alibh97@users.noreply.github.com>
2020-11-15 19:39:47 +0330

    norm and numpy

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+import math
+from numpy.linalg import norm
+import numpy as np
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -42,7 +44,22 @@ def initialization():
 # are sorted in descending order according to their static preference values,
 # and then the first L nodes are stored into a list of node i,
 # where L a sufficient large integer (in our experiment, L is chosen as 50).
-# def favorite_nodes(nodes):
+def favorite_nodes(nodes):
+    static_preference_values=[]
+
+    # for i in range(1,len(nodes)-1):
+    #
+
+# suppose current node is i ,static preference value of node j , is r_j / c_ij ,
+# r_j is the reward of node j , and c_ij is the travel time of edge(i,j)
+def static_preference_values(nodes,index):
+
+    for j in range(1,len(nodes)):
+        a=np.array([nodes[index][0],nodes[index][1]])
+        b=np.array([nodes[j][0],nodes[j][1]])
+        c_ij=norm(a-b)
+
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -51,8 +68,9 @@ if __name__ == '__main__':
     # P is the number of paths
     # Tmax is the available time budget per path
     # points is a list of nodes(points) with their x & y coordinates and scores
-    print('n= ', N,'\np= ',P,'\nTmax= ',Tmax,'\nPoints= ',Points)
 
+    # print('n= ', N,'\np= ',P,'\nTmax= ',Tmax,'\nPoints= ',Points)
+    print(norm(np.array([Points[0][0],Points[0][1]])-np.array([Points[1][0],Points[1][1]])))
 
     # line 1 in algorithm 1
     IS =[]  # incumbent solution needs to be empty at first, it's a set of solution(x)s
@@ -61,12 +79,12 @@ if __name__ == '__main__':
     Xb=[]  # the best so far solution
 
     # line 3 in algorithm 1
-    for i in range(N) :
-        # line 4 in algorithm 1
-        x = initialization()
-
-        # line 5 in algorithm 1
-        IS.append(x)
+    # for i in range(N) :
+    #     # line 4 in algorithm 1
+    #     x = initialization()
+    #
+    #     # line 5 in algorithm 1
+    #     IS.append(x)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
