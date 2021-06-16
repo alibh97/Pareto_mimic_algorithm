70f633ba8944eb2df3350442caf6a4c66ceca9be
alibh97 <alibh97@users.noreply.github.com>
2021-06-16 16:47:26 +0430

    relocate opearator added succesfully

--- main.py
+++ main.py
@@ -219,6 +219,49 @@ def mimic_operator(nodes, solution):
 
 # def local_search(nodes,solution):
 
+def relocate_operator(solution,nodes):
+    finding_better_solution=True
+    while finding_better_solution:
+        flag=False
+        for path1 in solution:
+            if flag:
+                break
+            travel_time_path1 = calculate_total_travel_time(path1, nodes)
+            for node1 in path1:
+                if flag:
+                    break
+                relocate_node=node1
+                current_path1=list(path1)
+                current_path1.remove(relocate_node)
+                new_travel_time_path1=calculate_total_travel_time(current_path1,nodes)
+                reduction_time_path1=travel_time_path1-new_travel_time_path1
+                for path2 in solution:
+                    if flag:
+                        break
+                    if path1!=path2:
+                        travel_time_path2 = calculate_total_travel_time(path2, nodes)
+                        for i in range(len(path2)+1):
+                            current_path2 = list(path2)
+                            current_path2.insert(i,relocate_node)
+                            new_travel_time_path2=calculate_total_travel_time(current_path2,nodes)
+                            increased_time_path2=new_travel_time_path2-travel_time_path2
+                            if increased_time_path2 < reduction_time_path1 and \
+                                    new_travel_time_path2 <= Tmax and \
+                                    new_travel_time_path1 <= Tmax:
+                                solution[solution.index(path1)]= current_path1
+                                solution[solution.index(path2)]= current_path2
+                                flag=True
+                                break
+                            else:
+                                if i == len(path2) and \
+                                        solution.index(path2) == (len(solution)-2) and \
+                                        path1.index(node1) == (len(path1)-1) and \
+                                        solution.index(path1) == (len(solution)-1):
+                                    finding_better_solution=False
+    return solution
+
+
+# cross move operator
 def cross_operator(solution, nodes):
     finding_better_solution = True
     while finding_better_solution:
@@ -538,6 +581,8 @@ if __name__ == '__main__':
 
     print('n= ', no_nodes, '\np= ', no_paths, '\nTmax= ', Tmax, '\nPoints= ', Points)
 
+
+
     # line 1 in algorithm 1
     IS = []  # incumbent solution needs to be empty at first, it's a set of solution(x)s
 
@@ -552,11 +597,14 @@ if __name__ == '__main__':
         print('round ', counter, 'x= ', x)
         x = mimic_operator(Points, x)
         print('mimic: ', x)
-        x = two_opt_operator(x, Points)
-        print('two: ',x)
-        x=exchange_operator(x, Points)
-        print('exch: ',x)
-        print('cros: ',cross_operator(x,Points))
+        two_opt_operator(x, Points)
+        print('two_opt: ',x)
+        exchange_operator(x, Points)
+        print('exchange: ',x)
+        cross_operator(x,Points)
+        print('cross: ',x)
+        relocate_operator(x,Points)
+        print('relocat: ',x)
         # line 5 in algorithm 1
         if not (IS.__contains__(x)):
             IS.append(x)
@@ -578,3 +626,9 @@ if __name__ == '__main__':
         #     # line 12 in algorithm 1
         #     x=
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def localSearch(solution,nodes):
+    two_opt_operator(solution,nodes)
+    exchange_operator(solution,nodes)
+    cross_operator(solution,nodes)
+    relocate_operator(solution,nodes)
