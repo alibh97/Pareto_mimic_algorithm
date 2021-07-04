c87e34bb68fdf51fdbcf50c8e56235fcb7326947
alibh97 <alibh97@users.noreply.github.com>
2021-07-04 15:48:26 +0430

    I just forgot I am in this branch and changed two opt of swallow operator

--- swallow/different_local_search/operators/two_opt.py
+++ swallow/different_local_search/operators/two_opt.py
@@ -1,28 +1,51 @@
-import swallow.different_local_search.functions as functions
+import local_search.functions as functions
+from file_content import no_nodes, no_paths, Tmax, Points,integer_parameter,similarity_ratio
 
-def different_two_opt_operator(solution,route,nodes):
+
+def two_opt_operator(solution,route_Rl, nodes):
+    index_of_route_Rl=solution.index(route_Rl)
+    route_Rl_travel_time=functions.calculate_total_travel_time(route_Rl,nodes)
     for path in solution:
-        smallest_travel_time = functions.calculate_total_travel_time(path, nodes)
-        no_nodes_to_swap = len(path)
         current_path = path
-        while True:
-            flag = False
-            c = 0
-            for i in range(no_nodes_to_swap - 1):
-                if flag:
-                    break
-                for k in range(i + 1, no_nodes_to_swap):
-                    new_path = functions.two_opt_swap(path, i, k)
-                    new_travel_time = functions.calculate_total_travel_time(new_path, nodes)
-                    if new_travel_time < smallest_travel_time:
-                        smallest_travel_time = new_travel_time
-                        solution[solution.index(current_path)] = new_path
-                        current_path = new_path
 
-                        flag = True
-                        break
-                c = i
-            if c == (no_nodes_to_swap - 2):
+        while True:
+            neighborhood = []
+            no_nodes_to_swap = len(current_path)
+
+            for i in range(no_nodes_to_swap - 1):
+
+                for k in range(i + 1, no_nodes_to_swap):
+                    neighboring_solution = list(solution)
+                    new_path = functions.two_opt_swap(current_path, i, k)
+
+                    new_travel_time = functions.calculate_total_travel_time(new_path, nodes)
+
+                    # if current path is route Rl,new path will be accepted if it's travel time is less than route Rl
+                    if solution.index(current_path)==index_of_route_Rl:
+                        neighboring_solution[solution.index(current_path)] = new_path
+
+                        neighborhood.append(neighboring_solution)
+
+                    # if new path is feasible , neighboring solution will be made , in this way we can keep the
+                    # feasibility of other routes than route Rl
+                    if new_travel_time <= Tmax:
+
+                        neighboring_solution[solution.index(current_path)] = new_path
+
+                        neighborhood.append(neighboring_solution)
+            # a neighboring solution is accepted if it is the first one that can shorten the travel time of route Rl
+            # and keep the feasibility of other routes.
+
+
+                best_neighboring_solution = neighborhood[0][0]
+
+                current_path = [k for k in best_neighboring_solution if k not in solution]
+                current_path=current_path[0]
+                solution[:] = best_neighboring_solution
+            else:
+                # Otherwise, the operator stops.
                 break
 
-    return solution
+
+
+
